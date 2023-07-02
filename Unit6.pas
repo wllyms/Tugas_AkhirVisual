88686963769fc5fd88641a1579500adf36cf8863
wllyms <smithwillyams778@gmail.com>
2023-07-02 12:46:47 +0700

    last update

---
+++ Unit6.pas
@@ -0,0 +1,24 @@
+unit Unit6;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TFprestasi = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Fprestasi: TFprestasi;
+
+implementation
+
+{$R *.dfm}
+
+end.
